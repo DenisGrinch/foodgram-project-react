b313456d87933225928521e4844b5930e8901a37
Denis Karpushin <grincharg@gmail.com>
2022-11-02 21:48:35 +0100

    isort fix

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import (MaxValueValidator, MinValueValidator,
-                                    RegexValidator, MinLengthValidator,
-                                    MaxLengthValidator)
+from django.core.validators import (MaxLengthValidator, MaxValueValidator,
+                                    MinLengthValidator, MinValueValidator,
+                                    RegexValidator)
 from django.db import models
 from django.db.models import UniqueConstraint
 
